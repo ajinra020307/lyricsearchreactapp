e233e08fdb6695dd3c65d8e94f083b74b623660b
ajinra020307 <ajinra020307@gmail.com>
2019-09-18 12:34:23 +0530

    finishedbuild

---
+++ precache-manifest.8bffacc73024cf24c35bb8d530dbcb79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca10ad400f9798a3c19e945920553ee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7bf01b047e339403950",
+    "url": "/static/css/main.9fa56dfd.chunk.css"
+  },
+  {
+    "revision": "cd75ad3d2e9098272afb",
+    "url": "/static/js/2.d135729b.chunk.js"
+  },
+  {
+    "revision": "d7bf01b047e339403950",
+    "url": "/static/js/main.19ea3a69.chunk.js"
+  },
+  {
+    "revision": "9eaa76e77d14d57d65a4",
+    "url": "/static/js/runtime~main.b41af966.js"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/spinner.75ae2ba2.gif"
+  }
+]);
